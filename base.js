1191f2273d0980ffabb76603a017552d36b225a5
Louisa Denison <louisa.denison@gmail.com>
2014-04-26 12:07:27 +0200

    Initial commit
    Website for Karl T. Ulrich’s class

---
+++ base.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+   animation: "slide",
+   directionNav: false,
+  });
+
+$(function() {
+$( '#dl-menu' ).dlmenu();
+});
+
+$(function() {
+  $(".drop").fitText(4);
+});
+
+
+});
